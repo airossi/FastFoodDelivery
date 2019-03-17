8eaf58cd5dad97f22c8acd3f000bf3c1049066e0
Damian Blanco <damian.blanco@globallogic.com>
2019-03-17 11:39:34 -0300

    Proof of concept: Order CRUD

---
+++ src/main/kotlin/com/example/FastFoodDelivery/FastFoodDeliveryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.FastFoodDelivery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastFoodDeliveryApplication
+
+fun main(args: Array<String>) {
+	runApplication<FastFoodDeliveryApplication>(*args)
+}
